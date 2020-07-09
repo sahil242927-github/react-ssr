a13b3b19829fd9fc2bef38a6d0bb04149b7b4bc0
Mohd Sahil <sahil242927@gmail.com>
2020-07-09 13:03:49 +0530

    fixed heroku

--- server/index.js
+++ server/index.js
@@ -30,6 +30,6 @@ app.get('*', (req, res, next) => {
   }
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('\nListening on port : ', PORT);
 });
